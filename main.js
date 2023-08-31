a236c64a53def87340d217462c66b9d344ed6721
JesseMann <personal.jessemann@gmail.com>
2023-08-30 21:35:30 -0500

    actually updated the mobile nav menu to by default hidden

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 function myFunction() {
     var x = document.getElementById("NavLinks");
-    if (x.style.display === "none") {
-      x.style.display = "flex";
-    } else {
+    if (x.style.display === "flex") {
       x.style.display = "none";
+    } else {
+      x.style.display = "flex";
     }
   }
   
